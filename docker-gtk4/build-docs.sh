28499ac061364748c2827e6c19313360c7c74712
Arjan Molenaar <gaphor@gmail.com>
2022-06-17 16:57:57 +0200

    Fix build for Fedora 36

--- docker-gtk4/build-docs.sh
+++ docker-gtk4/build-docs.sh
@@ -6,7 +6,7 @@ TAG="lazka/pgi-docgen:v4"
 
 podman run --security-opt label=disable \
     --rm  --volume "$(pwd)/..:/home/user/app" \
-    -t "${TAG}" ./pgi-docgen create _docs Gtk-4.0
+    -t "${TAG}" ./pgi-docgen create _docs Gtk-4.0 Adw-1
 
 podman run --security-opt label=disable \
     --rm  --volume "$(pwd)/..:/home/user/app" \
